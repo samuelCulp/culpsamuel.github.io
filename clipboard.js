565623826d1b73aeb1f19e80b8bea53ce6975c41
samuelCulp <99611615+samuelCulp@users.noreply.github.com>
2024-06-02 00:34:30 -0500

    Update clipboard.js

--- clipboard.js
+++ clipboard.js
@@ -1,8 +1,8 @@
-        function copyToClipboard(elementId) {
-            var copyText = document.getElementById(elementId).dataset.link;
-            navigator.clipboard.writeText(copyText).then(function() {
-                alert("Copied to clipboard: " + copyText);
-            }, function(err) {
-                alert("Failed to copy text: " + err);
-            });
-        }
+function copyToClipboard(elementId) {
+    var copyText = document.getElementById(elementId).dataset.link;
+    navigator.clipboard.writeText(copyText).then(function() {
+        alert("Copied to clipboard: " + copyText);
+    }, function(err) {
+        alert("Failed to copy text: " + err);
+    });
+}
